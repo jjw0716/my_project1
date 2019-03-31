530778cf8dd90855604d5baaaa7c4f4ccfd2ebe4
jjw0716 <48234467+jjw0716@users.noreply.github.com>
2019-03-31 21:25:06 +0900

    Update s3_web_server_insert_db.js

--- s3_web_server_insert_db.js
+++ s3_web_server_insert_db.js
@@ -41,4 +41,6 @@ app.get('/log', function(req, res) {
 var server = app.listen(8080, function () {
   var host = server.address().address
   var port = server.address().port
-                                                               
+    console.log('listening at http://%s:%s', host, port)
+});
+
