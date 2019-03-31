6ffe819fa168ea8b94605e59a2ac540fe8fa0d5d
jjw0716 <48234467+jjw0716@users.noreply.github.com>
2019-03-31 20:20:26 +0900

    Add files via upload

---
+++ my_h.h
@@ -0,0 +1,15 @@
+#ifndef my_h
+#define my_h
+
+#define port_1 80
+#define port_2 8080
+#define host_1 "api.thingspeak.com"
+#define host_2 "ec2-54-180-149-98.ap-northeast-2.compute.amazonaws.com"
+#define url_1 "/update?api_key=9OIPCK8DKGPEZVBX&field1="
+#define url_2 "/log?device=202&unit=3&type=T&value="
+#define WIFI_ID "V35 ThinQ_5984"
+#define WIFI_PW "253fef0a1a4e"
+
+
+
+#endif
